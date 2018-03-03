55640e3dc00797e0962a294b6f286eae7dfe6541
MOd <mate.modos@ge.com>
2018-03-03 11:55:23 +0100

    add controllers

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(8080, () => console.log(
+    'Webhook server is listening, port 8080'
+));
